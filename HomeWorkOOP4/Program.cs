21df9e90a4cde824cf555d4081e101bc133c8cef
StarGrim33 <prosky33@gmail.com>
2022-12-09 21:43:37 +0300

    1
    В процессе решения

---
+++ HomeWorkOOP4/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace HomeWorkOOP4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pack[] cards = { new Pack("7 червей"), new Pack("8 червей"), new Pack("Король червей"), new Pack("Туз червей") };
+            bool isProgramOn = true;
+
+            while (isProgramOn)
+            {
+                Console.WriteLine("Нажмите любую клавишу, чтобы взять карту");
+
+                for (int i = 0; i < cards.Length; i++)
+                {
+                    cards[i].ShowAllCards();
+                }
+
+
+            }
+        }
+    }
+}
+
+class Player
+{
+    public Player(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; private set; }
+
+    public void TakeCard()
+    {
+        //Console.WriteLine
+    }
+}
+
+class Pack
+{
+    public Pack(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; private set; }
+    public int CardsRemaining { get; private set; }
+
+    public void ShowAllCards()
+    {
+        Console.WriteLine("\nКолода карт: \nКарта - " + Name);
+        Console.ReadKey();
+    }
+
+}
