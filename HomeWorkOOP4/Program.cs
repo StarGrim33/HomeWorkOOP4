0bf2712324db619dbeb86d3dd465641643bfcdd0
StarGrim33 <prosky33@gmail.com>
2022-12-15 17:16:03 +0300

    Выполнение задачи

--- HomeWorkOOP4/Program.cs
+++ HomeWorkOOP4/Program.cs
@@ -4,19 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Pack[] cards = { new Pack("7 червей"), new Pack("8 червей"), new Pack("Король червей"), new Pack("Туз червей") };
+            const string CommandTakeCard = "1";
+            const string CommandShowAllCards = "2";
+            const string CommandShowMyCards = "3";
+            const string CommandExit = "4";
+
             bool isProgramOn = true;
+
+            Player player = new("Влад");
+            Deck deck = new Deck();
 
             while (isProgramOn)
             {
-                Console.WriteLine("Нажмите любую клавишу, чтобы взять карту");
+                Console.Write(player.Name);
 
-                for (int i = 0; i < cards.Length; i++)
+                Console.Clear();
+
+                Console.WriteLine($"{CommandTakeCard}-Взять карту");
+                Console.WriteLine($"{CommandShowAllCards}-Показать все карты");
+                Console.WriteLine($"{CommandShowMyCards}-Показать мои карты");
+                Console.WriteLine($"{CommandExit}-Выйти из программы");
+
+                string userInput = Console.ReadLine();
+                
+                switch (userInput)
                 {
-                    cards[i].ShowAllCards();
+                    case CommandTakeCard:
+                        player.TakeCard(deck);
+                        break;
+                    case CommandShowAllCards:
+                        deck.ShowAllCards();
+                        break;
+                    case CommandShowMyCards:
+                        player.ShowPlayerCards();
+                        break;
+                    case CommandExit:
+                        isProgramOn = false;
+                        break;
                 }
-
-
             }
         }
     }
@@ -31,26 +56,84 @@ class Player
 
     public string Name { get; private set; }
 
-    public void TakeCard()
+    private List<Cards> _cardsOnHand = new List<Cards>();
+
+    public void TakeCard(Deck deck)
     {
-        //Console.WriteLine
+        if(deck.TryGetCard(out Cards? cards))
+        {
+            _cardsOnHand.Add(cards);
+            Console.WriteLine("Вы взяли карту");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("Закончились карты в колоде");
+        }
+    }
+
+    public void ShowPlayerCards()
+    {
+        int minValueCard = 1;
+
+        if (_cardsOnHand.Count >= minValueCard)
+        {
+            for (int i = 0; i < _cardsOnHand.Count; i++)
+            {
+                Console.WriteLine($"Карта-{_cardsOnHand[i].Name}.Номинал-{_cardsOnHand[i].Value}");
+                Console.ReadKey();
+            }
+        }
+        else
+        {
+            Console.WriteLine("У вас сейчас нет карт на руках");
+            Console.ReadKey();
+        }
     }
 }
 
-class Pack
+class Deck
 {
-    public Pack(string name)
-    {
-        Name = name;
-    }
-
-    public string Name { get; private set; }
-    public int CardsRemaining { get; private set; }
+    public List<Cards> _cards = new List<Cards> { new Cards("6", "Черви"), new Cards("7", "Черви"), new Cards("8", "Черви"), new Cards("Король", "Черви")};
 
     public void ShowAllCards()
     {
-        Console.WriteLine("\nКолода карт: \nКарта - " + Name);
+        Console.Clear();
+        Console.WriteLine("Колода карт:");
+
+        for(int i = 0; i < _cards.Count; i++)
+        {
+            Console.WriteLine($"Карта - {_cards[i].Name}, номинал - {_cards[i].Value}");
+        }
+
         Console.ReadKey();
     }
 
+    public bool TryGetCard(out Cards? cards)
+    {
+        if(_cards.Count >= 1)
+        {
+            cards = _cards[^1];
+            _cards.Remove(cards);
+            return true;
+        }
+        else
+        {
+            cards = null;
+            Console.WriteLine("Ошибка");
+            return false;
+        }
+    }
+}
+
+class Cards
+{
+    public Cards(string name, string value)
+    {
+        Name = name;
+        Value = value;
+    }
+
+    public string Name { get;  set; }
+    public string Value { get;  set; }
 }
